f80d8e539bfca6d41c1c4947c2fa9ef42e0786bb
elboyran <e.ranguelova@esciencecenter.nl>
2018-11-29 14:07:02 +0100

    parameterised the script. Separated simple test case from generic use.

--- IntersectionOutput/multipoly_classes2shapefile.py
+++ IntersectionOutput/multipoly_classes2shapefile.py
@@ -7,11 +7,12 @@ Created on Wed Nov 28 17:23:46 2018
 """
 
 # imports
+import argparse
 import fiona
 from shapely.geometry import Polygon, MultiPolygon, mapping
 #from descartes.patch import PolygonPatch # for visualization
 #from matplotlib import pyplot # for plots
-from os.path import join
+from os.path import join, splitext
 
 # functions
 # visualization
@@ -70,73 +71,82 @@ def save_multipoly_and_classes2shapefile(multipolygon, classes, shapefilename):
 
 # Main
 def main():
-    # Define planar shapes by thier verticies
-    a = [(2,6), (4,6), (4,9), (2,9), (2,6)]
-    b = [(6,9), (7,6), (9,4),(9,8), (7,9),(6,9)]
-    c = [(1,1), (5,1),(5,4),(3,4),(1,1)]
-    d = [(6,1), (10,1),(7,3),(6,1)]
-    e = [(4,12), (6, 15), (8, 13), (4, 12)]
-    f = [(2,12), (3,14), (4, 13), (2,12)]
-
-    # multipolygon from coordinates
-    # multipoly = MultiPolygon([[a, []], [b, []] ,[c, []], [d, []]])
-
-    # convert shapes to polygons
-    #poly_a = Polygon(a)
-    #poly_b = Polygon(b)
-    #poly_c = Polygon(c)
-    poly_d = Polygon(d)
+    test = True # true for the artificial test case, for generig purpose, please, make False!
     
-    # multipolygon from polygons
-    #multi = []
-    #multi.append(poly_a)
-    #multi.append(poly_b)
-    #multi.append(poly_c)
-    #multi.append(poly_d)
-    #multipoly = MultiPolygon(multi)
+      
+    if test: # test case
+        # Define planar shapes by thier verticies
+        a = [(2,6), (4,6), (4,9), (2,9), (2,6)]
+        b = [(6,9), (7,6), (9,4),(9,8), (7,9),(6,9)]
+        c = [(1,1), (5,1),(5,4),(3,4),(1,1)]
+        d = [(6,1), (10,1),(7,3),(6,1)]
+        e = [(4,12), (6, 15), (8, 13), (4, 12)]
+        f = [(2,12), (3,14), (4, 13), (2,12)]
+
+        # create test list containing polygons and multipolygons    
+        poly_d = Polygon(d)       
+        multi_abc = MultiPolygon([[a, []], [b, []] ,[c, []]])
+        multi_ef = MultiPolygon([[e, []], [f, []]])
+        multi_poly_list = [multi_abc, poly_d, multi_ef]
+              
+        # Visualization parameters
+        '''
+        ORANGE = '#FF6600'
+        al = 0.8
+        show_verticies = True
+        extent = [0, 0, 11, 16] # format of extent is [xmin, ymin, xmax, ymax]
+        
+        # Display the multipolygon
+        fig = pyplot.figure(1, dpi=90)
+        ax = fig.add_subplot(111)
+        
+        show_multipolygon(multipoly, ax, show_verticies, extent, ORANGE, al, 'multipolygon')
+        
+        pyplot.show()
+        '''
+        classes = [4, 5, 4, 3, 3, 3]
+        
+        # filename
+        shapefilename = 'simple_example_multipolygon.shp'
+        
+        # full filename
+        path = '.'
+        full_shapefilename = join(path, shapefilename)
+        
+    else:
+        # argument parser
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', '--fname', help='File name with polygons and their classes', type=str)
+        parser.add_argument('-shpp', '--shapepath', help='Shape file path', type=str)
+        
+        # parse the input arguments arguments
+        args = parser.parse_args()
+        
+        # construct the shapefilename from the input filename
+        base_fname, ext = splitext(args.fname)
+        shapefilename = base_fname + '.' + 'shp'
+        full_shapefilename = join(args.shapepath, shapefilename)
+        
+        # parse the file and extract the list of (multi)polygons and their classes
+        multi_poly_list = []
+        classes = []
+        
+    # create a multipolygon from all (multi)polygons in the input list
+    multi = []
+    for el in multi_poly_list: # for all (potential) multipolygons
+        if el.geom_type == 'MultiPolygon':
+            for p in el: # for all polygons in each (potential) multipolygon
+                multi.append(p)
+        elif el.geom_type == 'Polygon':
+            multi.append(el) # just add a polygon
     
-     # multipolygon from collections of polygons and multipolygons
-    all_multi = []
-    multi_abc = MultiPolygon([[a, []], [b, []] ,[c, []]])
-    multi_ef = MultiPolygon([[e, []], [f, []]])
-    for p in multi_abc:
-        all_multi.append(p)
-    all_multi.append(poly_d)
-    for p in multi_ef:
-        all_multi.append(p)
-
-    multipoly = MultiPolygon(all_multi)
-
+    multipoly = MultiPolygon(multi)
+    
     print('Multipolygon is valid?: ', multipoly.is_valid)
-    
-    # Visualization parameters
-    '''
-    ORANGE = '#FF6600'
-    al = 0.8
-    show_verticies = True
-    extent = [0, 0, 11, 16] # format of extent is [xmin, ymin, xmax, ymax]
-    
-    # Display the multipolygon
-    fig = pyplot.figure(1, dpi=90)
-    ax = fig.add_subplot(111)
-    
-    show_multipolygon(multipoly, ax, show_verticies, extent, ORANGE, al, 'multipolygon')
-    
-    pyplot.show()
-    '''
-    classes = [4, 5, 4, 3, 3, 3]
-    
-    # filename
-    ext = 'shp'
-    fname = 'simple_example_multipolygon' + '.' + ext
-    
-    # full filename
-    path = '.'
-    shapefname = join(path, fname)
-    
     # saving to shape file (!)
-    save_multipoly_and_classes2shapefile(multipoly, classes, shapefname)
+    save_multipoly_and_classes2shapefile(multipoly, classes, full_shapefilename)
+    
+    print("Please, check with a GIS weather this file contains a valid polygon! \n", full_shapefilename)
 
-    print("Please, check with a GIS weather this file contains a valid polygon! \n", shapefname)
 if __name__ == '__main__':
     main()        
